4220ddd1aa5f9eaaf1d88e2afcd2ff8cc53b1f8e
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-02-03 14:52:59 -0300

    backend django frontend react

---
+++ backend/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import *
+
+class ReactView(APIView):
+    def get(self,request):
+        output = [{"employee": output.employee,
+                   "departament": output.department}
+                  for output in React.objects.all()]
+        return Response(output)
+        
+    def post(self, request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)            
+
+
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React from 'react';
+
+class App extends React.Component {
+
+  state = { details: [] }
+
+  componentDidMount(){
+
+    let data;
+    axios.get('http://localhost:8000/')
+    .then(res => {
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => {})
+  }
+
+  render() {
+    return (
+      <div>
+        <header> Data from Django displayed with React: </header>
+        <hr></hr>
+        {this.state.details.map((output, id) => (
+          <div key={id}>
+            <div>
+              <h2>{output.employee}</h2>
+              <h3>{output.departament}</h3>
+            </div>
+          </div>  
+        ))}
+      </div>
+    )
+  }
+}
+
+export default App;
